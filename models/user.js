c49c1efccfee029a2849beb4b91a76b88834cf2b
Simon Weller <s.meierbeck@gmail.com>
2017-08-27 22:35:50 +0200

    setting up mongooes database

--- models/user.js
+++ models/user.js
@@ -5,4 +5,24 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model('User', userSchema);
+
+User.statics.findOrCreate = (query, done) => {
+  User.find(query, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      user = new User(query);
+      user.save((err) => {
+        if (err) {
+          return done(err);
+        }
+        return done(err, user);
+      });
+    } else {
+      return done(err, user);
+    }
+  });
+}
+
 module.exports = User;
