e565b4bcef31c7a76fbe1f4d846a1dc94c46b328
Simon Weller <s.meierbeck@gmail.com>
2017-09-14 21:57:38 +0200

    tidy-up

--- routes/index.js
+++ routes/index.js
@@ -28,28 +28,31 @@ function checkAuth(req, res, next) {
     return next();
   }
   // We have a user - skip the authentification and update the bar model
-  Bar.findOne({yelpid: req.params.business}, (err, bar) => {
-    if (bar) {
-      const userIndex = bar.going.indexOf(req.user.twitter.username);
-      if (userIndex === -1) {
-        // not yet going
-        bar.going.push(req.user.twitter.username);
-      } else {
-        // already going
-        bar.going.splice(userIndex, 1);
-      }
-      bar.save(err => {console.error(err)})
-         .then(data => res.redirect(req.headers.referer));
-    } else {
-      const newBar = new Bar({
-        yelpid: req.params.business,
-        going: [req.user.twitter.username]
-      });
-      newBar.save(err => {console.error(err)})
-            .then(data => res.redirect(req.headers.referer));
-    }
-  });
-
+  Bar.findOne({yelpid: req.params.business})
+     .then(bar => {
+       if (bar) {
+         const userIndex = bar.going.indexOf(req.user.twitter.username);
+         if (userIndex === -1) {
+           // not yet going
+           bar.going.push(req.user.twitter.username);
+         } else {
+           // already going
+           bar.going.splice(userIndex, 1);
+         }
+         return bar.save()
+       } else {
+         const newBar = new Bar({
+           yelpid: req.params.business,
+           going: [req.user.twitter.username]
+         });
+         return newBar.save()
+       }
+     })
+     .then(data => res.redirect(req.headers.referer))
+     .catch(err => {
+       console.error(err);
+       res.redirect(req.headers.referer);
+     });
 }
 
 module.exports = router;
